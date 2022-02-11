a186496c47944f59abb16bb02e67ad5de4e99e13
Noémie Goeuriot <ngoeuriot14@gmail.com>
2022-02-11 15:10:43 +0100

    m

--- module0.py
+++ module0.py
@@ -1,1 +1,14 @@
-
+def multiply_by_two(a):
+    """
+    This function muliplies a number by 2
+    Args:
+    a(float): number
+    Returns:
+        float: result of the multiplication by 2
+    Examples:
+    >>> multiply_by_two(5)
+    10
+    >>> multiply_by_two(2.5)
+    5
+    """
+    return 2*a

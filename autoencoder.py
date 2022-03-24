7a1f57503f859ea2de9058ab9bdf8ee4565a796e
mathieu <mathieu.perrin@insa-lyon.fr>
2022-03-24 09:22:44 +0100

    Adding the autoencoder

---
+++ autoencoder.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # VAE for the CelebA dataset
+#
+# > In this post, we will implement the variational AutoEncoder (VAE) for an image dataset of celebrity faces. This is the Programming Assignment of lecture "Probabilistic Deep Learning with Tensorflow 2" from Imperial College London.
+#
+# - toc: true
+# - badges: true
+# - comments: true
+# - author: Chanseok Kang
+# - categories: [Python, Coursera, Tensorflow_probability, ICL]
+# - image: images/celeba-reconstruct.png
+
+# ## Packages
+
+
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Flatten, Dense, UpSampling2D, Reshape
+from tensorflow.keras.models import Model, Sequential
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# custom import
+from utils import *
+
+tfd = tfp.distributions
+tfpl = tfp.layers
+tfb = tfp.bijectors
+
+plt.rcParams['figure.figsize'] = (10, 6)
+
+
+# ## Mixture of Gaussians distribution
+#
+# We will define a prior distribution that is a mixture of Gaussians. This is a more flexible distribution that is comprised of $K$ separate Gaussians, that are combined together with some weighting assigned to each.
+#
+# Recall that the probability density function for a multivariate Gaussian distribution with mean $\mu\in\mathbb{R}^D$ and covariance matrix $\Sigma\in\mathbb{R}^{D\times D}$ is given by
+#
+# $$
+# \mathcal{N}(\mathbf{z}; \mathbf{\mu}, \Sigma) = \frac{1}{(2\pi)^{D/2}|\Sigma|^{1/2}}
+# \exp\left(-\frac{1}{2}(\mathbf{z}-\mathbf{\mu})^T\Sigma^{-1}(\mathbf{z}-\mathbf{\mu})\right).
+# $$
+#
+# A mixture of Gaussians with $K$ components defines $K$ Gaussians defined by means $\mathbf{\mu}_k$ and covariance matrices $\Sigma_k$, for $k=1,\ldots,K$. It also requires mixing coefficients $\pi_k$, $k=1,\ldots,K$ with $\sum_{k} \pi_k = 1$. These coefficients define a categorical distribution over the $K$ Gaussian components. To sample an event, we first sample from the categorical distribution, and then again from the corresponding Gaussian component.
+#
+# The probability density function of the mixture of Gaussians is simply the weighted sum of probability density functions for each Gaussian component:
+#
+# $$
+# p(\mathbf{z}) = \sum_{k=1}^K \pi_k \mathcal{N}(\mathbf{z}; \mathbf{\mu}_k, \Sigma_k)
+# $$
+
+# ## Define the prior distribution
+#
+# We will define the mixture of Gaussians distribution for the prior, for a given number of components and latent space dimension. Each Gaussian component will have a diagonal covariance matrix. This distribution will have fixed mixing coefficients, but trainable means and standard deviations.
+
+
+def get_prior(num_modes, latent_dim):
+    """
+    Creates a mixture of gaussian distribution.
+
+
+    Args:
+        num_modes (_type_): _description_
+        latent_dim (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    prior = tfd.MixtureSameFamily(
+        # The mixture distribution is a list of probability for the distributions. Here it is set to be
+        # all equal
+        mixture_distribution=tfd.Categorical(
+            probs=[1 / num_modes, ] * num_modes),
+        # The components distribution is made of two lists : loc represents the mean of the distribution,
+        # or the "location" of the distribution.
+        # scale_diag represents the
+        components_distribution=tfd.MultivariateNormalDiag(
+            loc=tf.Variable(tf.random.normal(shape=[num_modes, latent_dim])),
+            scale_diag=tfp.util.TransformedVariable(tf.Variable(
+                tf.ones(shape=[num_modes, latent_dim])), bijector=tfb.Softplus())
+        )
+    )
+    return prior
+
+
+# Define the encoder Network
+#
+# We will now define the encoder network as part of the VAE. First, we will define the `KLDivergenceRegularizer` to use in the encoder network to add the KL divergence part of the loss.
+
+
+def get_kl_regularizer(prior_distribution):
+    divergence_regularizer = tfpl.KLDivergenceRegularizer(
+        prior_distribution,
+        use_exact_kl=False,
+        weight=1.0,
+        test_points_fn=lambda q: q.sample(3),
+        test_points_reduce_axis=(0, 1)
+    )
+    return divergence_regularizer
+
+
+def get_encoder(latent_dim, kl_regularizer):
+    encoder = Sequential([
+        Conv2D(32, (4, 4), activation='relu', strides=2,
+               padding='SAME', input_shape=(64, 64, 3)),
+        BatchNormalization(),
+        Conv2D(64, (4, 4), activation='relu', strides=2, padding='SAME'),
+        BatchNormalization(),
+        Conv2D(128, (4, 4), activation='relu', strides=2, padding='SAME'),
+        BatchNormalization(),
+        Conv2D(256, (4, 4), activation='relu', strides=2, padding='SAME'),
+        BatchNormalization(),
+        Flatten(),
+        Dense(tfpl.MultivariateNormalTriL.params_size(latent_dim)),
+        tfpl.MultivariateNormalTriL(
+            latent_dim, activity_regularizer=kl_regularizer)
+    ])
+    return encoder
+
+
+# ## Define the decoder network
+#
+# We'll define the decoder network for the VAE, which return IndependentBernoulli
+# distribution of `event_shape=(64, 64, 3)`
+
+def get_decoder(latent_dim):
+    decoder = Sequential([
+        Dense(4096, activation='relu', input_shape=(latent_dim, )),
+        Reshape((4, 4, 256)),
+        UpSampling2D(size=(2, 2)),
+        Conv2D(128, (3, 3), activation='relu', padding='SAME'),
+        UpSampling2D(size=(2, 2)),
+        Conv2D(64, (3, 3), activation='relu', padding='SAME'),
+        UpSampling2D(size=(2, 2)),
+        Conv2D(32, (3, 3), activation='relu', padding='SAME'),
+        UpSampling2D(size=(2, 2)),
+        Conv2D(128, (3, 3), activation='relu', padding='SAME'),
+        Conv2D(3, (3, 3), padding='SAME'),
+        Flatten(),
+        tfpl.IndependentBernoulli(event_shape=(64, 64, 3))
+    ])
+    return decoder
+
+
+# #### Define the average reconstruction loss
+#
+# You should now define the reconstruction loss that forms the remaining part of the negative ELBO objective. This function should take a batch of images of shape `(batch_size, 64, 64, 3)` in the first argument, and the output of the decoder after passing the batch of images through `vae` in the second argument.
+#
+# The loss should be defined so that it returns
+# $$
+#     -\frac{1}{n}\sum_{i=1}^n \log p(x_i|z_i)
+# $$
+# where $n$ is the batch size and $z_i$ is sampled from $q(z|x_i)$, the encoding distribution a.k.a. the approximate posterior. The value of this expression is always a scalar.
+#
+# Expression (1) is, as you know, is an estimate of the (negative of the) batch's average expected reconstruction loss:
+#
+# $$
+#     -\frac{1}{n}\sum_{i=1}^n \mathrm{E}_{Z\sim q(z|x_i)}\big[\log p(x_i|Z)\big]
+# $$
+
+
+def reconstruction_loss(batch_of_images, decoding_dist):
+    """
+    The function takes batch_of_images (Tensor containing a batch of input images to
+    the encoder) and decoding_dist (output distribution of decoder after passing the 
+    image batch through the encoder and decoder) as arguments.
+    The function should return the scalar average expected reconstruction loss.
+    """
+    return -tf.reduce_mean(decoding_dist.log_prob(batch_of_images), axis=0)
+
+
+# ## Compute reconstructions of test images
+#
+# We will now take a look at some image reconstructions from the encoder-decoder architecture.
+#
+# You should complete the following function, that uses `encoder` and `decoder` to reconstruct images from the test dataset. This function takes the encoder, decoder and a Tensor batch of test images as arguments. The function should be completed according to the following specification:
+#
+# * Get the mean of the encoding distributions from passing the batch of images into the encoder
+# * Pass these latent vectors through the decoder to get the output distribution
+#
+# Your function should then return the mean of the output distribution, which will be a Tensor of shape `(batch_size, 64, 64, 3)`.
+
+
+def reconstruct(encoder, decoder, batch_of_images):
+    """
+    The function takes the encoder, decoder and batch_of_images as inputs, which
+    should be used to compute the reconstructions.
+    The function should then return the reconstructions Tensor.
+    """
+    approx_posterior = encoder(batch_of_images)
+    decoding_dist = decoder(approx_posterior.mean())
+    return decoding_dist.mean()
+
+
+def plot_reconstructions_test_ds(encoder, decoder):
+
+    n_reconstructions = 7
+    num_test_files = np.load('./img_align_celeba/dataset_test.npy').shape[0]
+    test_ds_for_reconstructions = load_dataset('dataset_test')
+    # Run your function to compute reconstructions of random samples from the test dataset
+    for all_test_images, _ in test_ds_for_reconstructions.batch(num_test_files).take(1):
+        all_test_images_np = all_test_images.numpy()
+    example_images = all_test_images_np[np.random.choice(
+        num_test_files, n_reconstructions, replace=False)]
+
+    reconstructions = reconstruct(encoder, decoder, example_images).numpy()
+
+    # Plot the reconstructions
+
+    f, axs = plt.subplots(2, n_reconstructions, figsize=(16, 6))
+    axs[0, n_reconstructions // 2].set_title("Original test images")
+    axs[1, n_reconstructions // 2].set_title("Reconstructed images")
+    for j in range(n_reconstructions):
+        axs[0, j].imshow(example_images[j])
+        axs[1, j].imshow(reconstructions[j])
+        axs[0, j].axis('off')
+        axs[1, j].axis('off')
+
+    plt.tight_layout()
+
+    return None
+
+
+# ## Sample new images from the generative model
+#
+# Now we will sample from the generative model; that is, first sample latent vectors from the prior, and then decode those latent vectors with the decoder.
+#
+# You should complete the following function to generate new images. This function takes the prior distribution and decoder network as arguments, as well as the number of samples to generate. This function should be completed according to the following:
+#
+# * Sample a batch of `n_samples` images from the prior distribution, to obtain a latent vector Tensor of shape `(n_samples, 50)`
+# * Pass this batch of latent vectors through the decoder, to obtain an Independent Bernoulli distribution with batch shape equal to `[n_samples]` and event shape equal to `[64, 64, 3]`.
+#
+# The function should then return the mean of the Bernoulli distribution, which will be a Tensor of shape `(n_samples, 64, 64, 3)`.
+
+
+def generate_images(prior, decoder, n_samples):
+    """
+    The function takes the prior distribution, decoder and number of samples as inputs, which
+    should be used to generate the images.
+    The function should then return the batch of generated images.
+    """
+    z = prior.sample(n_samples)
+    return decoder(z).mean()
+
+
+# Run your function to generate new images
+
+def plot_generate_images(prior, decoder):
+    n_samples = 10
+    sampled_images = generate_images(prior, decoder, n_samples)
+
+    f, axs = plt.subplots(1, n_samples, figsize=(16, 6))
+
+    for j in range(n_samples):
+        axs[j].imshow(sampled_images[j])
+        axs[j].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('image_generation.png')
+
+
+# ## Modify generations with attribute vector
+#
+# We will see how the latent space encodes high-level information about the images, even though it has not been trained with any information apart from the images themselves.
+#
+# As mentioned in the introduction, each image in the CelebA dataset is labelled according to the attributes of the person pictured.
+
+
+# Function to load labels and images as a numpy array
+
+def load_labels_and_image_arrays(split):
+    dataset = load_dataset(split)
+    num_files = np.load('./img_align_celeba/{}.npy'.format(split)).shape[0]
+
+    for all_images, _ in dataset.batch(num_files).take(1):
+        all_images_np = all_images.numpy()
+
+    labels = pd.read_csv(
+        './img_align_celeba/list_attr_celeba.txt', delimiter=r"\s+", header=1)
+    #labels = labels[labels['image_id'].isin(files)]
+    return labels[:num_files], all_images_np
+
+
+##========================= MAIN =======================##
+
+if __name__ == "__main__":
+    print("Tensorflow Version: ", tf.__version__)
+    print("Tensorflow Probability Version: ", tfp.__version__)
+
+    #train_ds, val_ds, test_ds = load_datasets()
+
+    # display_examples(train_ds)
+
+    # Run your function to get the prior distribution with 2 components and latent_dim = 50
+    prior = get_prior(num_modes=2, latent_dim=50)
+
+    # Run your function to get the encoder
+    encoder = get_encoder(
+        latent_dim=50, kl_regularizer=get_kl_regularizer(prior))
+
+    # Print the encoder summary
+    # encoder.summary()
+    # tf.keras.utils.plot_model(encoder)
+
+    # Run your function to get the decoder
+    decoder = get_decoder(latent_dim=50)
+
+    # Print the decoder summary
+    # decoder.summary()
+    # tf.keras.utils.plot_model(decoder)
+
+    # ## Link the encoder and decoder together
+    #
+    # Connects `encoder` and `decoder` to form the end-to-end architecture.
+    vae = Model(inputs=encoder.inputs, outputs=decoder(encoder.outputs))
+
+    # ## Compile and fit the model
+    #
+    # It's now time to compile and train the model. Note that, it is recommand to use Hardware accelerator while training.
+
+    # Compile the model
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+    vae.compile(optimizer=optimizer, loss=reconstruction_loss)
+
+    # Train the model
+    #vae.fit(train_ds, validation_data=val_ds, epochs=1)
+    # Or just load the weights
+    encoder.load_weights("./model_vae/encoder/saved_encoder")
+    decoder.load_weights("./model_vae/decoder/saved_decoder")
+
+    # Evaluate the model on the test set
+    #test_loss = vae.evaluate(test_ds)
+    #print("Test loss: {}".format(test_loss))
+
+    #plot_reconstructions_test_ds(encoder, decoder)
+
+    plot_generate_images(prior, decoder)
---
+++ utils.py
@@ -0,0 +1,158 @@
+import numpy as np
+import cv2
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def center_crop(image, out_height, out_width):
+    """
+
+    Args:
+        image(numpy ndarray): list of floats representing the pixels of an image
+        out_height(int) : height targeted
+        out_width(int) : width targeted
+    Return:
+        image(numpy ndarray) : image of size out_height*out_width
+    """
+    input_height, input_width = image.shape[:2]
+    offset_height = (input_height - out_height) // 2
+    offset_width = (input_width - out_width) // 2
+    image = image[offset_height:offset_height+out_height,
+                  offset_width:offset_width+out_width, :]
+    return image
+
+
+def resize_maintain_aspect(image, target_h, target_w):
+    """resize the image while maintaining the proportions
+
+    Args:
+        image (numpy ndarray): list of floats representing the pixels of an image
+        target_h (int): targeted height
+        target_w (int): targeted width
+
+    Returns:
+        ndarray: resized image
+    """
+    input_height, input_width = image.shape[:2]
+    if input_height > input_width:
+        new_width = target_w
+        new_height = int(input_height*(target_w/input_width))
+    else:
+        new_height = target_h
+        new_width = int(input_width*(target_h/input_height))
+
+    image = cv2.resize(image, (new_width, new_height),
+                       interpolation=cv2.INTER_CUBIC)
+    return image
+
+
+def save_dataset(path, imageList, height, width, channels, out):
+    """method to save a list of images from a directory into a binary numpy array
+    file .npy
+
+    Args:
+        path (string): path to the directory where the images are
+        imageList (list<string>): list of the targeted images
+        height (int): targeted height
+        width (int): targeted width
+        channels (int): color channels
+        out (string): location where the file will be saved
+    """
+    # placeholder arrays for data and labels
+    # data is float32, labels are integers
+    x = np.ndarray(shape=(len(imageList), height,
+                   width, channels), dtype=np.float32)
+
+    # loop through all images
+    for i in range(len(imageList)):
+        # open image to numpy array
+        img = cv2.imread(path + imageList[i])
+
+        # do all the pre-processing…
+        img = resize_maintain_aspect(img, height, width)
+        img = center_crop(img, height, width)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = (img/255.0).astype(np.float32)
+
+        # insert into placeholder array
+        x[i] = img
+
+    # write placeholder arrays into a binary npy file
+    np.save(out, x)
+
+    return None
+
+
+# Acknowledgement to https://github.com/foolmarks/images_to_npy/blob/8b85c5d40346b7bf60ac340e42faa2bc5508b0cf/img_to_npy.py#L19
+
+
+def load_dataset(split):
+    """method to load the dataset from a .npy file with the 'split' name
+
+    Args:
+        split (string): names of the dataset targeted
+
+    Returns:
+        tensorflow.python.data.ops.dataset_ops.BatchDataset: Tensorflow Dataset containing the images
+    """
+    if os.path.exists('./img_align_celeba/{}.npy'.format(split)):
+        list_ds = tf.data.Dataset.from_tensor_slices(
+            np.load('./img_align_celeba/{}.npy'.format(split)))
+        ds = list_ds.map(lambda x: (x, x))
+        return ds
+    else:
+        print("save the data first")
+        return None
+
+
+def load_datasets():
+    path = "../img_align_celeba/training/"
+
+    files = os.listdir(path)
+
+    imageList = files[0:10000]
+    imageList_val = files[10001:11000]
+    imageList_test = files[11001:12000]
+
+    height = 64
+    width = 64
+    channels = 3
+    out_train = './img_align_celeba/dataset.npy'
+    out_val = './img_align_celeba/dataset_val.npy'
+    out_test = './img_align_celeba/dataset_test.npy'
+
+    if os.path.exists(out_test):
+        save_dataset(path, imageList_test, height, width, channels, out_test)
+    if os.path.exists(out_val):
+        save_dataset(path, imageList_val, height, width, channels, out_val)
+
+    # Load the training, validation and testing datasets splits
+    train_ds = load_dataset('dataset')
+    val_ds = load_dataset('dataset_val')
+    test_ds = load_dataset('dataset_test')
+
+    # Batch the Dataset objects
+
+    batch_size = 32
+    train_ds = train_ds.batch(batch_size)
+    val_ds = val_ds.batch(batch_size)
+    test_ds = test_ds.batch(batch_size)
+
+    return train_ds, val_ds, test_ds
+
+
+def display_examples(ds):
+    """Display a few examples
+
+    Args:
+        ds (Dataset): dataset from which we want the examples
+    """
+    n_examples_shown = 6
+    f, axs = plt.subplots(1, n_examples_shown, figsize=(16, 3))
+
+    for j, image in enumerate(ds.take(n_examples_shown)):
+        axs[j].imshow(image[0])
+        axs[j].axis('off')
+
+    return None

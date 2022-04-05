7ee3a127f3b83537de36439a074ff8b14ceb86c7
mathieu <mathieu.perrin@insa-lyon.fr>
2022-04-05 17:29:49 +0200

    Architecture changes #3

--- project4bim2022_g2/main.py
+++ project4bim2022_g2/main.py
@@ -1,7 +1,7 @@
 from autoencoder import *
 #from Traitement_Database import *
 #from utils import *
-from Genetic_algorithm import *
+from project4bim2022_g2.Genetic_algorithm import *
 
 import numpy as np
 import matplotlib.image as mpimg
@@ -9,18 +9,16 @@ import matplotlib.pyplot as plt
 #import tensorflow as tf
 
 
-
-#On construit l'encodeur et le décodeur et on charge les poids calculés lors de l'entrainement du réseau de neurones
+# On construit l'encodeur et le décodeur et on charge les poids calculés lors de l'entrainement du réseau de neurones
 prior = get_prior(num_modes=2, latent_dim=50)
 encoder = get_encoder(latent_dim=50, kl_regularizer=get_kl_regularizer(prior))
 decoder = get_decoder(latent_dim=50)
 
-#encoder.load_weights("VAE_encoder2")
-#decoder.load_weights("VAE_decoder2")
+# encoder.load_weights("VAE_encoder2")
+# decoder.load_weights("VAE_decoder2")
 
 encoder.load_weights("./model_vae/encoder/saved_encoder")
 decoder.load_weights("./model_vae/decoder/saved_decoder")
-
 
 
 #

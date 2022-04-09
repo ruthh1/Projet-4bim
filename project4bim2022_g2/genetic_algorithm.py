e6d45bd45bdd58d3c4b9a049cb77ec2158d400f2
Noemie-Goeuriot <99486510+Noemie-Goeuriot@users.noreply.github.com>
2022-04-09 23:11:08 +0200

    Changing docstrings

--- project4bim2022_g2/genetic_algorithm.py
+++ project4bim2022_g2/genetic_algorithm.py
@@ -8,12 +8,14 @@ from project4bim2022_g2.autoencoder import *
 # All function definition
 
 def generation_copies(X, n):
-    '''This function generates n copies of the parent X (that will serve to create the final children)
+    '''
+    This function generates n copies of the parent X (that will serve to create the final children)
     Args:
         X (np.array) = vector of the latent space of a parent
         n (int) = number of copies we want from this parent
-    Return :
-        A np.array containing n copies of X '''
+    Return:
+        np.array: np.array containing n copies of X
+     '''
     X2 = list(X)
     Y = []
     for i in range(n):
@@ -22,13 +24,15 @@ def generation_copies(X, n):
 
 
 def mutation_un_vecteur(parent, step_size, p):
-    ''' This function applies a mutation on each element of the latent_vector in each subnp.array of the list Y containing copies of the same parent or recombinations of parents
+    '''
+    This function applies a mutation on each element of the latent_vector in each subnp.array of the list Y containing copies of the same parent or recombinations of parents
     Args:
         parent (np.array) : a vector of the latent space
         step_size (float) : parameter of the mutation
         p (float) : probability (between 0 and 1) that there is a mutation for each element of the latent vector
-    Return :
-        x (np.array) : vecteur modifié muté (avec une probabilité p de mutation pour chaque élément)'''
+    Return:
+        np.array:vecteur modifié muté (avec une probabilité p de mutation pour chaque élément)
+    '''
     n = len(parent)
     for i in range(n):
         if np.random.rand() < p:
@@ -38,13 +42,15 @@ def mutation_un_vecteur(parent, step_size, p):
 
 
 def mutation(Y, step_size, p):
-    ''' This function applies a mutation on each vector contained in Y
+    '''
+    This function applies a mutation on each vector contained in Y
     Args:
         Y (np.array) : np.array of vectors (latent vectors)
         step_size (float) : parameter of the mutation
         p (float) : probability (between 0 and 1) that there is a mutation for each element of the latent vector
-    Return :
-        Y (np.array) : the modification is made directly on Y, and at the end, Y contains len(Y) latent vectors, modified by mutation'''
+    Return:
+        np.array:the modification is made directly on Y, and at the end, Y contains len(Y) latent vectors, modified by mutation
+    '''
     n = len(Y)
     for i in range(n):
         Y[i] = mutation_un_vecteur(Y[i], step_size, p)
@@ -52,14 +58,16 @@ def mutation(Y, step_size, p):
 
 
 def recombination_2parents_moy(X1, X2, p):
-    '''Calcule le vecteur obtenu à partir d'une recombinaison à partir des 2 parents X1 et X2, qui calcule la moyenne des coordonnées pour chaque élément des vecteurs latents, avec une probabilité p pour chaque élément
-        Sinon, c'est l'un ou l'autre des éléments de X1 ou X2 qui est choisi
-    Args :
+    '''
+    Calcule le vecteur obtenu à partir d'une recombinaison à partir des 2 parents X1 et X2, qui calcule la moyenne des coordonnées pour chaque élément des vecteurs latents, avec une probabilité p pour chaque élément
+    Sinon, c'est l'un ou l'autre des éléments de X1 ou X2 qui est choisi
+    Args:
         X1 (np.array/list): vecteur de l'espace latent, un parent
         X2 (np.array/list) : vecteur de l'espace latent, un parent
         p (float) : probabilité de modifier l'élément à chaque tour de boucle (p entre 0 et 1, si p vaut 1, new_vector est la moyenne de X1 et X2, si p vaut 0, les éléments de new_vector sont extraits soit de X1, soit de X2)
-    Return :
-        new_vector(np.array) : np.array formant un nouveau vecteur de l'espace latent, avec pour certaines coordonnées celles de X1, pour d'autres celles de X2, pour d'autres la moyenne des 2'''
+    Return:
+        np.array:np.array formant un nouveau vecteur de l'espace latent, avec pour certaines coordonnées celles de X1, pour d'autres celles de X2, pour d'autres la moyenne des 2
+    '''
     n = len(X1)
     new_vector = []
     for i in range(n):
@@ -75,13 +83,15 @@ def recombination_2parents_moy(X1, X2, p):
 
 
 def recombination_k_parents(X, p):
-    '''Calcule le vecteur obtenu à partir d'une recombinaison à partir des k parents de la liste X, qui calcule la moyenne des coordonnées pour chaque élément des vecteurs latents, avec une probabilité p pour chaque élément
-        Sinon, on choisit un élément parmi les k vecteurs
-    Args :
+    '''
+    Calcule le vecteur obtenu à partir d'une recombinaison à partir des k parents de la liste X, qui calcule la moyenne des coordonnées pour chaque élément des vecteurs latents, avec une probabilité p pour chaque élément
+    Sinon, on choisit un élément parmi les k vecteurs
+    Args:
         X (np.array): Liste de vecteurs de l'espace latent, de longueur k
         p (float) : probabilité de modifier l'élément à chaque tour de boucle (p entre 0 et 1, si p vaut 1, new_vector est la moyenne de X1 et X2, si p vaut 0, les éléments de new_vector sont extraits soit de X1, soit de X2)
-    Return :
-        new_vector(np.array) : np.array formant un nouveau vecteur de l'espace latent, recombinaison des k parents'''
+    Return:
+        np.array:np.array formant un nouveau vecteur de l'espace latent, recombinaison des k parents
+    '''
     k = len(X)
     new_vector = []
     n = len(X[0])
@@ -103,14 +113,16 @@ def recombination_k_parents(X, p):
 
 
 def one_iteration_mutation(List_parents, type_evol, n, step_size):
-    '''This function calculates the next generation of children from a list of parents. The children are generated only by mutation here, according to the type of evolution str_images_chosen
-    Args :
+    '''
+    This function calculates the next generation of children from a list of parents. The children are generated only by mutation here, according to the type of evolution str_images_chosen
+    Args:
         List_parents (np.array/list) : list of parents that we want to generate children the next generation
         type_evol (str) : type of evolution, '+' if we want the parent to be part of the next generation, ',' otherwise
         n (int) : number of children we want to be generated from each parents
         step_size (float) : parameter of mutation
-    Return :
-        List (np.array) of latent vectors that are the children of the parents in List_parents, length should be n*len(List_parents) if type_evol == ',', and (n+1)*len(List_parents) otherwise'''
+    Return:
+        np.array:list of latent vectors that are the children of the parents in List_parents, length should be n*len(List_parents) if type_evol == ',', and (n+1)*len(List_parents) otherwise
+    '''
     L_children = []
     for parent in List_parents:
         if type_evol == '+':
@@ -128,14 +140,15 @@ def one_iteration_mutation(List_parents, type_evol, n, step_size):
 
 
 def adjust_children(List_parents, prior):
-    '''This function creates a list of 10 children from List_parents, thanks to the function mutation et recombination, depending on len(List_parents)
-        It also adds to the children generated from the parents random images generated by the autoencoder, so that the user can always change the direction of search of the algorithm
+    '''
+    This function creates a list of 10 children from List_parents, thanks to the function mutation et recombination, depending on len(List_parents)
+    It also adds to the children generated from the parents random images generated by the autoencoder, so that the user can always change the direction of search of the algorithm
     Args:
         List_parents (np.array) : List of the images selected by the user from which we want to generate new images
         prior : previously constructed for the neural network, necessary to generate new images from the autoencoder
     Return:
-        L (np.array) :
-        '''
+        np.array:list of 10 children (dont 2 generated images randomly by the neurol network)
+    '''
     n = len(List_parents)
     if n == 1:
         L = one_iteration_mutation(List_parents, ',', 4, 1.5)
